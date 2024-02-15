b28ba37b7c7f56a2a391049cd1ad8013e0e006a2
lilikas11 <lilianapcribeiro@gmail.com>
2024-02-15 09:48:51 +0000

    maven project

---
+++ lab1/lab1_1/src/test/java/lab1/lab1_1/Lab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package lab1.lab1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
